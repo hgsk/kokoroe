d75bd98b9fa0b775df183e72c3ddaa41f9d2dcac
hgsk <hgsk85@gmail.com>
2015-07-08 01:41:52 +0900

    Resolved #5 赤字で警告

--- action.php
+++ action.php
@@ -6,7 +6,12 @@
 </head>
 <body>
     <p>
-        input: <?php echo htmlspecialchars($_GET['input']); ?>
+        input:
+        <?php if ('' == $_GET['input']) : ?>
+            <span style="color: red;">文字列が入力されていません</span>
+        <?php else : ?>
+            <span><?php echo htmlspecialchars($_GET['input']); ?></span>
+        <?php endif ?>
     </p>
 
     <a href="/">戻る</a>
